4c07ead6af70cb5decd3254d2af52a2e81a89854
JingWei-S <weijing.524@gmail.com>
2023-03-05 12:25:30 +1100

    add the main content and search bar

--- src/index.js
+++ src/index.js
@@ -16,19 +16,19 @@ date_content.textContent = formattedDate;
 main_content.appendChild(date_content);
 
 const main_temp = document.createElement('div');
-main_temp.textContent = '31';
+main_temp.textContent = '31°C';
 main_temp.classList.add('main-temp');
 main_content.appendChild(main_temp);
 
 const weather_desc = document.createElement('div');
-weather_desc.textContent = 'Cloudy';
+weather_desc.textContent = 'Cloudy ';
 const icon_span = document.createElement('span');
 icon_span.textContent = '☁️';
 weather_desc.appendChild(icon_span);
 main_content.appendChild(weather_desc);
 
 const feel_temp = document.createElement('div');
-feel_temp.textContent = '35';
+feel_temp.textContent = 'Feels like 35°C';
 main_content.appendChild(feel_temp);
 
 const bi_temp = document.createElement('div');
@@ -37,9 +37,17 @@ const lo_temp = document.createElement('div');
 const hi_temp = document.createElement('div');
 bi_temp.appendChild(lo_temp);
 bi_temp.appendChild(hi_temp);
-lo_temp.textContent = 'L: 12';
-hi_temp.textContent = 'H: 33';
+lo_temp.textContent = 'L: 12°C';
+hi_temp.textContent = 'H: 33°C';
 main_content.appendChild(bi_temp);
+
+// set up the search bar
+const search_div = document.querySelector('.search');
+const search_input = document.createElement('input');
+search_input.classList.add('search_input');
+search_input.setAttribute('placeholder', 'Melbourne');
+search_input.setAttribute('type', 'text');
+search_div.appendChild(search_input);
 
 
 // async function getWeather() {
